759d0fc38069a81ff4ff27de7e1d008039a452a9
Yahaya Ben Zamey <79756073+Benaxo@users.noreply.github.com>
2023-07-31 13:47:06 +0200

    token removed

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,5 @@ export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn,
-  token: "sksh3QsO38b7NW7xqXeVUcOWnOs9MBqkEr8pMaFPKRIJ4SK3BbKMGRE6Iqrale35NB6LB8Myf52gDz5z9rny6q1zP2G8zBOvtG0ewPU93C8UOFF9CVQ5BrJdGlJWhnobR8oFLf4Rwhnjo98MdQQsvGZA47kOSFTSDc27AgEjZWK7MzXNpzH3"
+  useCdn
 })
