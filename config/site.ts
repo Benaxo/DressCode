533f09822812ed8745e157cb775a071a68f75287
Yahaya Ben Zamey <79756073+Benaxo@users.noreply.github.com>
2023-07-31 13:46:39 +0200

    final init

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "DressCode",
+  description: "Définissez qui vous voulez être avec nos vêtements et accessoires pour toutes les personnalitées",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
